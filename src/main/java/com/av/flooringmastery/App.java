692ce675f84148830c33b188bd122801539c1c58
ArisaBonsaiTree <angelevarela@hotmail.com>
2023-03-06 00:48:10 -0800

    Added Spring to the program

--- src/main/java/com/av/flooringmastery/App.java
+++ src/main/java/com/av/flooringmastery/App.java
@@ -7,17 +7,14 @@ import com.av.flooringmastery.service.FlooringMasteryServiceLayerImpl;
 import com.av.flooringmastery.ui.FlooringMasteryView;
 import com.av.flooringmastery.ui.UserIO;
 import com.av.flooringmastery.ui.UserIOConsoleImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 
 public class App {
     public static void main(String[] args) {
-
-        UserIO io = new UserIOConsoleImpl();
-        FlooringMasteryView view = new FlooringMasteryView(io);
-
-        FlooringMasteryDao dao = new FlooringMasteryDaoImpl();
-        FlooringMasteryServiceLayerImpl serviceLayer = new FlooringMasteryServiceLayerImpl(dao);
-        FlooringMasteryController controller = new FlooringMasteryController(view, serviceLayer);
-
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        FlooringMasteryController controller = ctx.getBean("controller", FlooringMasteryController.class);
         controller.run();
     }
 }
